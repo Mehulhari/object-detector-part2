42000bb3bd5fa98810985978a66c1301ca1da23c
Mehulhari <111678646+Mehulhari@users.noreply.github.com>
2022-11-16 18:36:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+Status=""
+
+function setup(){
+   canvas= createCanvas(900,800);
+ 
+      canvas.position(300,100);
+}
+function preload(){
+Image="ppppp.jpeg"
+}
+function gotResult(){
+   
+   objects[i].width;
+   objects[i].height;
+   (objects[i].x , objects[i].y);
+   text(objects[i].label);
+   object[i].confidence;
+}
+ 
